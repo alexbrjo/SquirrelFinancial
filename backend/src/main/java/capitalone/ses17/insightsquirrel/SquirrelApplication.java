e92e853677cc0afbd55f404cfe036e84a6306314
alexbrjo <alexjo5000@gmail.com>
2017-08-17 11:37:07 -0400

    Renaming packaging

--- backend/src/main/java/capitalone/ses17/insightsquirrel/SquirrelApplication.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/SquirrelApplication.java
@@ -1,4 +1,4 @@
-package com.squirrel;
+package capitalone.ses17.insightsquirrel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
