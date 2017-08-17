5d5ff2fe67fabcd99ad0a637bb0298c2e2f2aae8
potatoshane <li2137@purdue.edu>
2017-08-17 11:18:15 -0400

    Intial Commit

---
+++ backend/src/main/java/com/squirrel/SquirrelApplication.java
@@ -0,0 +1,12 @@
+package com.squirrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirrelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquirrelApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/squirrel/SquirrelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.squirrel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SquirrelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
