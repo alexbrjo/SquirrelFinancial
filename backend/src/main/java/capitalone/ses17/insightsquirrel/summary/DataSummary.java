b24ec075b3b093264f33976edae318f5f281ccb7
alexbrjo <alexjo5000@gmail.com>
2017-08-17 13:03:21 -0400

    Adding ranges

--- backend/src/main/java/capitalone/ses17/insightsquirrel/summary/DataSummary.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/summary/DataSummary.java
@@ -1,5 +1,7 @@
 package capitalone.ses17.insightsquirrel.summary;
 
+import java.util.Date;
+
 /**
  * Summary of a set of transaction data.
  *
@@ -8,5 +10,29 @@ package capitalone.ses17.insightsquirrel.summary;
 public interface DataSummary {
 
     public String getSummaryName();
+
+    public DateRange getRangeStart();
+
+    public DateRange getRangeEnd();
+
+    public static class DateRange {
+
+        private Date start;
+
+        private Date end;
+
+        public DateRange (Date start, Date end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public Date getStart () {
+            return start;
+        }
+
+        public Date getEnd(){
+            return end;
+        }
+    }
 
 }
--- backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SummaryMaker.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SummaryMaker.java
@@ -32,4 +32,6 @@ public class SummaryMaker {
         return null;
     }
 
+    public void processTransactions() {}
+
 }
