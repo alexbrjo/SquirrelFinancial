359fa24fd87846d096f598e6ec2a0edc59ab9cec
lec4347 <lec4347@cs.rit.edu>
2017-08-17 15:53:00 -0400

    Updated speech responses

--- backend/src/main/java/capitalone/ses17/insightsquirrel/speech/ResponseController.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/speech/ResponseController.java
@@ -1,17 +1,18 @@
 package capitalone.ses17.insightsquirrel.speech;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 
-import capitalone.ses17.insightsquirrel.summary.SummaryMaker;
+import capitalone.ses17.insightsquirrel.summary.*;
 import com.jayway.jsonpath.JsonPath;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ResponseController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
+    @Autowired
+    private SummaryMaker summaryMaker;
     /*
     POST endpoint that receives JSON data and formulates a response to send to Alexa.
     Default response is null if no response can be created.
@@ -22,63 +23,50 @@ public class ResponseController {
         String intent = JsonPath.read(payload, "$.request.intent.name");
         //SummaryMaker instance = new SummaryMaker();
         String response = "TEST";
-        /*
+
         switch (intent) {
             case "Budget":
-                response = budgetResponse(payload);
+                BudgetSummary budgetSummary = null;
+                response = budgetResponse(budgetSummary);
                 break;
             case "Data":
-                response = dataResponse(payload);
+                DataSummary dataSummary = summaryMaker.getAllSummary();
+                response = dataResponse(dataSummary);
                 break;
             case "Location":
-                response = locationResponse(payload);
+                LocationSummary locationSummary = null;
+                response = locationResponse(locationSummary);
                 break;
             case "Schedule":
-                response = scheduleResponse(payload);
-                break;
-            case "Credit":
-                response = creditResponse(payload);
-                break;
-            case "CreditCard":
-                response = creditCardResponse(payload);
+                List<ScheduleSummary> scheduleSummary = summaryMaker.getScheduleSummaries();
+                response = scheduleResponse(scheduleSummary);
                 break;
             default:
                 break;
         }
-        */
-        return intent;
+        return response;
     }
 
-    public String budgetResponse(String payload) {
+    public String budgetResponse(BudgetSummary summary) {
 
         String response = "You have spent " + " percent of your budget on food, " + " percent on ";
         return response;
     }
 
-    public String dataResponse(String payload) {
+    public String dataResponse(DataSummary summary) {
         String r = null;
         return r;
     }
 
-    public String locationResponse(String payload) {
-        String totalSpent = JsonPath.read(payload, "$.request.intent.slots");
-        String location = JsonPath.read(payload, "$.request.intent.slots");
-        String response = "You spent " + totalSpent + " dollars in ";
+    public String locationResponse(LocationSummary summary) {
+
+        String response = "You spent " +  " dollars in ";
         return response;
     }
 
-    public String scheduleResponse(String payload) {
+    public String scheduleResponse(List<ScheduleSummary> summary) {
+
         String r = null;
         return r;
-    }
-
-    public String creditResponse(String payload) {
-        String response = "Credit allows you to buy something now with an agreement to pay for it later. This can be in the form of loans or credit cards";
-        return response;
-    }
-
-    public String creditCardResponse(String payload){
-        String response = "A credit card allows you to use credit to make purchases up to a maximum credit limit. You can keep a balance and pay the money back over time while paying interest.";
-        return response;
     }
 }
