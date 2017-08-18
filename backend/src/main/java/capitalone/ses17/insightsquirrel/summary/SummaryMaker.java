b400ee3143daaefd3da714544916b0068fe89f8d
alexbrjo <alexjo5000@gmail.com>
2017-08-17 20:44:29 -0400

    Implemented jsonpath

--- backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SummaryMaker.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SummaryMaker.java
@@ -3,17 +3,14 @@ package capitalone.ses17.insightsquirrel.summary;
 import capitalone.ses17.insightsquirrel.elastic.ElasticController;
 import capitalone.ses17.insightsquirrel.summary.model.Transaction;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
+import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import sun.util.calendar.CalendarDate;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * Creates summaries of transactions to be consumed by the core API
@@ -29,42 +26,38 @@ public class SummaryMaker {
     public Summary getSummary(double radius, String location,
                               Date start, Date end, String category) {
 
-       /* GeoApiContext context = new GeoApiContext.Builder()
+        GeoApiContext context = new GeoApiContext.Builder()
                 //.apiKey("YOUR-KEY-HERE")
                 .build();
 
         GeocodingResult[] results = null;
         try {
             results = GeocodingApi.geocode(context, location).await();
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            System.out.println(gson.toJson(results[0].addressComponents));
         } catch (Exception e) {
-
+            // yikes
         }
 
-        double latitude = results[0].geometry.location.lat;
-        double longitude = results[0].geometry.location.lng;
+        double latitude = 35.787743;   // results[0].geometry.location.lat;
+        double longitude = -78.644257; // results[0].geometry.location.lng;
 
         String json = elasticController.getTimeLocationCategoy(start, end, latitude, longitude, category);
 
-        // parse json
-        Gson gson = new Gson();
-        Transaction transaction = gson.fromJson(json, Transaction.class);
-
         // transform data
-*/
         Summary summary = new Summary();
 
-        summary.category = "food";
+        summary.category = JsonPath.read(json, "$.hits.hits[0]._source.merchant.category[0]").toString();
         summary.humanizedDate = "the last week";
-        summary.total = 500;
-        summary.fromDate = new Date(1499659200000L);
-        summary.toDate = new Date(1498881600000L);
-        summary.location = "Arlington";
+        summary.total = ((Double) JsonPath.read(json, "$.hits.hits[0]._source.purchase.amount")).doubleValue();
+        summary.fromDate = new Date(1499659200000L); // JsonPath.read(json, "$.transactions[0].purchase.purchase_date
+        summary.toDate = new Date(1498881600000L);   // JsonPath.read(json, "$.transactions[0].purchase.purchase_date
+        summary.location = location;
 
         return summary;
     }
 
-
+    public static void main (String[] args) {
+        SummaryMaker s = new SummaryMaker();
+        s.getSummary(15, "raleigh",  new Date(), new Date(), "");
+    }
 
 }
