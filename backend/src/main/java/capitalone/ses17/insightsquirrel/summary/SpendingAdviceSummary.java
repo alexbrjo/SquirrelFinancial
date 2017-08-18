fdedd66e5dfa93b00779967ff4bf21ab35e02677
alexbrjo <alexjo5000@gmail.com>
2017-08-18 02:39:21 -0400

    top budget thing

--- backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SpendingAdviceSummary.java
+++ backend/src/main/java/capitalone/ses17/insightsquirrel/summary/SpendingAdviceSummary.java
@@ -15,11 +15,9 @@ public class SpendingAdviceSummary {
 
     public SpendingAdviceSummary(String json, String fromDate, String toDate) {
 
-        if (category != null) {
-            this.category = category;
-        }
+        this.category = JsonPath.read(json, "$.aggregations.2.buckets.key");
         this.purchases = ((Integer) JsonPath.read(json, "$.hits.total")).intValue();
-        this.total = ((Double) JsonPath.read(json, "$.aggregations.1.value")).doubleValue();
+        this.total = ((Double) JsonPath.read(json, "$.aggregations.2.buckets.1.value")).doubleValue();
         this.fromDate = fromDate;
         this.toDate = toDate;
     }
